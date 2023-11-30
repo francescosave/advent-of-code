b74afee6e843014ae022ba4e10d845854e168d77
Francesco ricci <francescosav.ricci@libero.it>
2023-11-30 19:51:24 +0100

    add 2023 folder and day1 template

---
+++ 2023/day_01_name_challenge/README.md
@@ -0,0 +1,9 @@
+## Day 01 -  
+
+Your puzzle answer was  `00000`.
+
+###  Part Two 
+
+Your puzzle answer was  `00000`.
+
+
---
+++ 2023/day_01_name_challenge/solution.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+
+// read input file
+let content_page = fs.readFileSync('input.txt', 'utf8')
+
+//let content_page = document.documentElement.innerText;
+let lines = content_page.split('\n');
+
+
+// functio of solution 1th star
+function star1(lines) {
+
+    var result = 0;
+
+    for (index = 0; index < lines.length - 1; index++) {
+        let element = lines[index];
+
+        // code for star 1
+
+    }
+
+    return result;
+
+}
+
+
+// functio of solution 2th star
+function star2(lines) {
+
+
+
+    var result = 0;
+
+
+    for (index = 0; index < lines.length - 1; index++) {
+        let element = lines[index];
+
+        // code for star 2
+
+    }
+
+
+    return result;
+
+}
+
+
+console.log('star1 ' + star1(lines));
+console.log('STAR2 ' + star2(lines));
